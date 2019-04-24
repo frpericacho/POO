7e036bbba1f5415c17b55a31fdea955fe8e9516a
frpericacho <frpericacho@gmail.com>
2019-04-24 11:26:21 +0200

    :rice_ball:

--- P2/usuario.cpp
+++ P2/usuario.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 #include "usuario.hpp"
 
-Usuario::Id_duplicado::Id_duplicado(const Cadena& cad):id_(cad){}
+Usuario::Id_duplicado::Id_duplicado(const Cadena &cad) : id_(cad) {}
 
-Cadena Usuario::Id_duplicado::idd() const{
+Cadena Usuario::Id_duplicado::idd() const
+{
     return id_;
 }
 
-Usuario::Usuario(const Cadena& id, const Cadena& nomb,const Cadena& apell,const Cadena& dir,const Clave& clv):
-identificador_(id), nombre_(nomb), apellidos_(apell), dirección_(dir),clave_(clv){}
+Usuario::Usuario(const Cadena &id, const Cadena &nomb, const Cadena &apell, const Cadena &dir, const Clave &clv) : identificador_(id), nombre_(nomb), apellidos_(apell), dirección_(dir), clave_(clv) {}
+
+void Usuario::es_titular_de(Tarjeta &){
+
+}
+void Usuario::no_es_titular_de(Tarjeta &){
+
+}
+
+Cadena Usuario::id() const{
+    return identificador_;
+}
+
+Cadena Usuario::nombre() const{
+    return nombre_;
+}
+
+Cadena Usuario::apellidos() const{
+    return apellidos_;
+}
+
+Cadena Usuario::direccion() const{
+    return dirección_;
+}
+
+//const Tarjeta &tarjetas() const{          HAY QUE PONER LAS TARJETAS
+//  return t_;
+//}          
+void Usuario::compra(Articulo &, unsigned){
+
+}
+
+const Articulo &Usuario::compra() const{
+
+}
+
+size_t Usuario::n_articulos() const{
+
+}
--- P2/usuario.hpp
+++ P2/usuario.hpp
@@ -7,20 +7,38 @@
 #include "numero.hpp"
 #include "articulo.hpp"
 
-class Usuario{
+class Tarjeta;
+
+class Usuario
+{
+public:
+    Usuario(const Cadena &, const Cadena &, const Cadena &, const Cadena &, const Clave &);
+
+    class Id_duplicado
+    {
     public:
-        Usuario(const Cadena& , const Cadena& ,const Cadena& ,const Cadena& ,const Clave& );
-        class Id_duplicado{
-            public:
-                Id_duplicado(const Cadena&);
-                Cadena idd() const;
-            private:
-                Cadena id_;
-        };
+        Id_duplicado(const Cadena &);
+        Cadena idd() const;
 
     private:
-        Cadena identificador_, nombre_, apellidos_, dirección_;
-        Clave clave_;
+        Cadena id_;
+    };
+
+    void es_titular_de(Tarjeta &);
+    void no_es_titular_de(Tarjeta &);
+    Cadena id() const;
+    Cadena nombre() const;
+    Cadena apellidos() const;
+    Cadena direccion() const;
+    //const Tarjeta &tarjetas() const;          HAY QUE PONER LAS TARJETAS
+    void compra(Articulo &, unsigned);
+    const Articulo &compra() const;
+    size_t n_articulos() const;
+
+    private:
+    Cadena identificador_,nombre_,apellidos_,dirección_;
+    Clave clave_;
+    //Tarjeta t_;        HAY QUE PONER LAS TARJETAS
 };
 
 #endif
